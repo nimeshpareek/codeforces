5c6cc3cbf1946cdfde690dc572e38d0af7663a8b
Nimesh Pareek <72460673+nimeshpareek@users.noreply.github.com>
2023-01-23 09:08:45 +0530

    Update petya_string.cpp

--- petya_string.cpp
+++ petya_string.cpp
@@ -1,34 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int n;
-    cin >> n;
-
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-        arr[i] = arr[i] - 1;
+int check(string s, string s1){
+    for(int i=0;i<s.size();i++){
+        if(s[i]-'a'>s1[i]-'a'){
+            return 1;
+        }else if(s[i]-'a'<s1[i]-'a'){
+            return -1;
+        }
     }
-
-    // 1 2 3 0
-    //
-    //  for(int i=0;i<n;i++){
-    //     cout << "arr is "<< arr[i] << " ";
-    // }
-    int temp[n];
-    // cout << endl;
-    for (int i = 0; i < n; i++)
-    {
-        int x = arr[i];
-        temp[x] = i + 1;
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        cout << temp[i] << " ";
-    }
-
     return 0;
+}
+int main() {
+	    string str,str2;
+	    cin>>str;
+        cin>>str2;
+	     transform(str.begin(), str.end(), str.begin(), ::tolower);
+         transform(str2.begin(), str2.end(), str2.begin(), ::tolower);
+         int c = check(str,str2);
+	    cout<<c<<endl;
+	return 0;
 }
